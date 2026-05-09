30ede6e2840e2ffca535a16cf9c5616bd0d90acf
bhmossberg <benjamin.h.mossberg@gmail.com>
2026-05-09 19:51:54 -0400

    Final Version

--- src/banking/BankAccount.java
+++ src/banking/BankAccount.java
@@ -1,79 +1,84 @@
 package banking;
 
 public class BankAccount {
-	// Required Fields
-	protected String firstName;
-	protected String lastName;
-	protected int accountID;
-	protected double balance;
-	
-	// Default constructor
-	public BankAccount() {
-		this.firstName = "First Name";
-		this.lastName = "Last Name";
-		this.accountID = -1;
-		this.balance = 0.0;
-	}
-	
-	// Parameterized constructor
-	public BankAccount(String firstName, String lastName, int accountID, double balance) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.accountID = accountID;
-		this.balance = balance;
-	}
-	
-	// Deposit funds
-	public void deposit(double amount) {
+    // Required Fields
+    protected String firstName;
+    protected String lastName;
+    protected int accountID;
+    protected double balance;
 
-		this.balance += amount;
-		System.out.printf("$%.2f depositted successfully.%nYou have $%.2f remaining.%n%n", amount, this.balance);
-	}
-	
-	// Withdraw funds
-	public void withdrawal(double amount) {
-		if (this.balance >= amount) {
-			this.balance -= amount;
+    // Default constructor
+    public BankAccount() {
+        this.firstName = "";
+        this.lastName = "";
+        this.accountID = -1;
+        this.balance = 0.0;
+    }
 
-			System.out.printf("$%.2f withdrawn successfully.%n", amount);
-		}
-		else {
-			System.out.println("Transaction cancelled, insufficient funds.");
-		}
-		System.out.printf("You have $%.2f remaining.%n", this.balance);
-		System.out.println();
-	}
-	
-	// Display Account information
-	public void accountSummary() {
-		System.out.println("====Displaying Account Summary====");
-		System.out.printf("Memer name: %s, %s%n", getLastName(), getFirstName());
-		System.out.printf("Account ID: %d%n", getAccountID());
-		System.out.printf("Current Balance: $%.2f%n", getBalance());
-		System.out.println();
-	}
-	
-	// Getters and Setters
-	public void setFirstName(String name) {
-		this.firstName = name;
-	}
-	public String getFirstName() {
-		return this.firstName;
-	}
-	public void setLastName(String name) {
-		this.lastName = name;
-	}
-	public String getLastName() {
-		return this.lastName;
-	}
-	public void setAccountID(int accountID) {
-		this.accountID = accountID;
-	}
-	public int getAccountID() {
-		return this.accountID;
-	}
-	public double getBalance() {
-		return this.balance;
-	}
-	
+    // Parameterized constructor
+    public BankAccount(String firstName, String lastName, int accountID, double balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountID = accountID;
+        this.balance = balance;
+    }
+
+    // Deposit funds
+    public void deposit(double amount) {
+        this.balance += amount;
+        System.out.printf("$%.2f deposited successfully.%nYou have $%.2f remaining.%n%n", 
+                         amount, this.balance);
+    }
+
+    // Withdraw funds
+    public void withdrawal(double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            System.out.printf("$%.2f withdrawn successfully.%n", amount);
+        } else {
+            System.out.println("Insufficient funds, transaction cancelled.");
+        }
+        System.out.printf("You have $%.2f remaining.%n%n", this.balance);
+    }
+
+    // Display Account information
+    public void accountSummary() {
+        System.out.println("==== Account Summary ====");
+        System.out.printf("Member name: %s, %s%n", getLastName(), getFirstName());
+        System.out.printf("Account ID: %d%n", getAccountID());
+        System.out.printf("Current Balance: $%.2f%n", getBalance());
+    }
+
+    // Getters and Setters
+    public void setFirstName(String name) {
+        this.firstName = name;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setLastName(String name) {
+        this.lastName = name;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public int getAccountID() {
+        return this.accountID;
+    }
+    
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    
+    public double getBalance() {
+        return this.balance;
+    }
 }
--- src/banking/CheckingAccount.java
+++ src/banking/CheckingAccount.java
@@ -1,35 +1,45 @@
 package banking;
 
 public class CheckingAccount extends BankAccount {
-	private double interestRate;
-	// Allow overdraft with $30 fee
-	@Override
-	public void withdrawal(double amount) {
-		if (this.balance >= amount) {
-			this.balance -= amount;
-			System.out.printf("$%.2f withdrawn successfully.", amount);
-		}
-		else {
-			processWithdrawal(amount);
-		}
-		System.out.printf("You have $%.2f remaining.%n%n", this.balance);
-	}
-	
-	public CheckingAccount() {
-		interestRate = 1.2;
-	}
-	
-	// Method for helping with overdraft
-	public void processWithdrawal(double amount) {
-		this.balance -= amount;
-		this.balance -= 30;
-		System.out.println("Overdraft detected! A $30 fee will be assessed");
-		System.out.printf("$%.2f withdrawn successfully.%n", amount);
-	}
-	public void setInterestRate(double rate) {
-		this.interestRate = rate;
-	}
-	public double getInterestRate() {
-		return this.interestRate;
-	}
+    private double interestRate;
+
+    // Default constructor
+    public CheckingAccount() {
+        super(); 
+        this.interestRate = 1.2;
+    }
+
+    // Overridden withdrawal with overdraft
+    @Override
+    public void withdrawal(double amount) {
+        if (getBalance() >= amount) {
+            super.withdrawal(amount);
+        } else {
+            processWithdrawal(amount);
+        }
+    }
+
+    // Process overdraft with $30 fee
+    public void processWithdrawal(double amount) {
+        double newBalance = getBalance() - amount - 30;
+        setBalance(newBalance);
+        System.out.println("Overdraft detected! A $30 fee will be assessed.");
+        System.out.printf("$%.2f withdrawn successfully.%n", amount);
+        System.out.printf("You have $%.2f remaining.%n%n", getBalance());
+    }
+
+    // Display checking account information 
+    public void displayAccount() {
+        accountSummary();
+        System.out.printf("Interest Rate: %.2f%%%n%n", interestRate);
+    }
+
+    // Getters and Setters for interestRate
+    public void setInterestRate(double rate) {
+        this.interestRate = rate;
+    }
+
+    public double getInterestRate() {
+        return this.interestRate;
+    }
 }
